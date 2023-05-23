bcc618081c544d26ba6a6cb4ca5726dedf9b4720
Jet Lin <jetlin101@gmail.com>
2023-05-23 14:25:07 -0700

    added temp nav and main

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main (){
+    return (
+        <div className="main">
+            <button className="main-button">Awan</button>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Nav (){
+    return (
+        <div className="nav">
+            <h1>Awan.ai</h1>
+            
+            <ul className="nav-items">
+                <li>
+                    <p>About Us</p>
+                </li>
+                <li>
+                    <p>Contact Us</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
